c0ac85259c1f16cae1c61dfe10e56e7e9d08aa8c
wave49192 <sirawich.tu@ku.th>
2021-11-06 12:06:07 +0700

    Add Attributes to Movie

--- movie.py
+++ movie.py
@@ -1,14 +1,26 @@
+from typing import List
+
+
 class Movie:
     """
     A movie available for rent.
     """
+
     # The types of movies (price_code).
-    def __init__(self, title):
+    def __init__(self, title: str, year: int, genre: List[str]):
         # Initialize a new movie.
         self.title = title
+        self.year = year
+        self.genre = genre
 
     def get_title(self):
         return self.title
+
+    def get_year(self):
+        return self.year
+
+    def get_genre(self):
+        return self.genre
 
     def __str__(self):
         return self.title
